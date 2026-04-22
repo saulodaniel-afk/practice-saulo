01b8a9b49ea238082ee735863ffc551c8eac6db9
saulo.daniel@WPA087TJ84F3N.corp.tvglobo.com.br <saulo.daniel@MacBook-Pro-de-Saulo.local>
2026-04-22 14:20:06 -0300

    practice-7

--- practice-7/onboard-globo-script.sh
+++ practice-7/onboard-globo-script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for file in ~/Downloads/*; do
+
+
+for file in $(find ~/Downloads -maxdepth 1 -type f -print0 | xargs -0 ls -1tr | head -10); do
     if [ -f "$file" ]; then
         echo "Arquivo: $(basename "$file")"
         read -p "Deseja (a)pagar, (m)over ou (p)ular? [a/m/p]: " acao
@@ -21,4 +23,18 @@ for file in ~/Downloads/*; do
         esac
     fi
 done
+
+# comando aprendido: find ~/Downloads -maxdepth 1 -type f -print0 | xargs -0 ls -1tr | head -10
+#find: Comando para buscar arquivos no sistema de arquivos.
+#-maxdepth 1: Limita a busca ao diretório atual, sem entrar em subdiretórios.
+#-type f: Especifica que queremos apenas arquivos (não diretórios).
+#-print0: Imprime os resultados separados por um caractere nulo, útil para
+#xargs: recebe saídas de outros comandos (via pipe |) e as converte em argumentos para um novo comando
+#-0: Lê a entrada separada por nulos
+#ls -1tr: Lista os arquivos em formato de uma linha, ordenados por data de modificação (mais antigos primeiro).
+#-t: Ordena os arquivos por data de modificação (mais recentes primeiro).
+#-r: Inverte a ordenação (mais antigos primeiro, já que -t sozinho seria mais recentes primeiro).
+#head -10: Exibe apenas os 10 primeiros arquivos da lista resultante.       
+
+
 
